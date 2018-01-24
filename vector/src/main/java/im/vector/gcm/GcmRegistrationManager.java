e3d7a311bc3807b3e44869de9d86ab6f7b037662
David Baker <dave@matrix.org>
2018-01-24 21:32:41 +0100

    Update GCM app id

--- vector/src/main/java/im/vector/gcm/GcmRegistrationManager.java
+++ vector/src/main/java/im/vector/gcm/GcmRegistrationManager.java
@@ -70,7 +70,7 @@ public final class GcmRegistrationManager {
     private static final String PREFS_SYNC_TIMEOUT = "GcmRegistrationManager.PREFS_SYNC_TIMEOUT";
     private static final String PREFS_SYNC_DELAY = "GcmRegistrationManager.PREFS_SYNC_DELAY";
 
-    private static final String DEFAULT_PUSHER_APP_ID = "im.vector.app.android";
+    private static final String DEFAULT_PUSHER_APP_ID = "fr.gouv.rie.tchap.android";
     private static final String DEFAULT_PUSHER_URL = "https://matrix.org/_matrix/push/v1/notify";
     private static final String DEFAULT_PUSHER_FILE_TAG = "mobile";
 
