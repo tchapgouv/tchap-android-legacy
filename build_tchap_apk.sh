32f38135a7f4d4cd0d8d9acd393cf1a1334fd61f
Giom Foret <giom@matrix.org>
2018-02-01 16:26:21 +0100

    Update release candidate apk version

--- build_tchap_apk.sh
+++ build_tchap_apk.sh
@@ -3,6 +3,8 @@ rm *.apk
 ./gradlew clean 
 
 ./gradlew lintAppRelease assembleAppMatrixorg
+./gradlew lintAppfdroidRelease assembleAppfdroidMatrixorg
 
 #cp app/build/outputs/apk/app-alpha-matrixorg.apk ./alpha.apk
 cp vector/build/outputs/apk/vector-app-matrixorg.apk ./tchapGooglePlay.apk
+cp vector/build/outputs/apk/vector-appfdroid-matrixorg.apk ./tchapFDroid.apk
