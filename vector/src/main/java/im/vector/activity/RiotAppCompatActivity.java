4e1053dd44bcab790aefba146cbe035eabf8c78f
audrey.olivier <audreyo@matrix.org>
2018-03-23 14:11:59 +0100

    Remove DINSIC Copyright

--- vector/src/main/java/im/vector/activity/RiotAppCompatActivity.java
+++ vector/src/main/java/im/vector/activity/RiotAppCompatActivity.java
@@ -1,6 +1,6 @@
 /*
  * Copyright 2015 OpenMarket Ltd
- * Copyright 2018 DINSIC
+ *
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
