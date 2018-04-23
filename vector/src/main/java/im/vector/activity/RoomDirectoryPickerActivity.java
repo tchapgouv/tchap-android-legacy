9e5b828b18ed44dd5603fbc7d34a1f74e540d696
audrey.olivier <audreyo@matrix.org>
2018-04-23 17:59:42 +0200

    Display i and a room directories for "a" and "i" users and nothing for "e" users

--- vector/src/main/java/im/vector/activity/RoomDirectoryPickerActivity.java
+++ vector/src/main/java/im/vector/activity/RoomDirectoryPickerActivity.java
@@ -17,23 +17,14 @@
 package im.vector.activity;
 
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
-
 import org.matrix.androidsdk.util.Log;
-
-import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
 
 import org.matrix.androidsdk.MXSession;
 import org.matrix.androidsdk.rest.callback.ApiCallback;
@@ -119,22 +110,11 @@ public class RoomDirectoryPickerActivity extends RiotAppCompatActivity implement
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_directory_server_picker, menu);
-        CommonActivityUtils.tintMenuIcons(menu, ThemeUtils.getColor(this, R.attr.icon_tint_on_dark_action_bar_color));
-        return true;
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
             setResult(RESULT_CANCELED);
             finish();
             return true;
-        }
-
-        if (item.getItemId() == R.id.action_add_custom_hs) {
-            displayCustomDirectoryDialog();
         }
 
         return super.onOptionsItemSelected(item);
@@ -154,21 +134,13 @@ public class RoomDirectoryPickerActivity extends RiotAppCompatActivity implement
         mSession.getEventsApiClient().getThirdPartyServerProtocols(new ApiCallback<Map<String, ThirdPartyProtocol>>() {
             private void onDone(List<RoomDirectoryData> list) {
                 stopWaitingView();
-                String userHSName = mSession.getMyUserId().substring(mSession.getMyUserId().indexOf(":") + 1);
                 String userHSUrl = mSession.getHomeServerConfig().getHomeserverUri().getHost();
 
                 List<String> hsUrlsList = Arrays.asList(getResources().getStringArray(R.array.room_directory_servers));
 
                 int insertionIndex = 0;
 
-                // Add user's HS
-                list.add(insertionIndex++, RoomDirectoryData.getIncludeAllServers(mSession, userHSUrl, userHSName));
 
-                // Add user's HS but for Matrix public rooms only
- /*               if (!list.isEmpty()) {
-                    list.add(insertionIndex++, RoomDirectoryData.getDefault());
-                }
-*/
                 // Add custom directory servers
                 for (String hsURL : hsUrlsList) {
                     if (!TextUtils.equals(userHSUrl, hsURL)) {
@@ -176,7 +148,13 @@ public class RoomDirectoryPickerActivity extends RiotAppCompatActivity implement
                     }
                 }
 
-                mRoomDirectoryAdapter.updateDirectoryServersList(list);
+                // sanity check
+                if (LoginActivity.isUserExternal(mSession)) {
+                    list.clear();
+                    mRoomDirectoryAdapter.updateDirectoryServersList(list);
+                } else {
+                    mRoomDirectoryAdapter.updateDirectoryServersList(list);
+                }
             }
 
             @Override
@@ -214,63 +192,6 @@ public class RoomDirectoryPickerActivity extends RiotAppCompatActivity implement
         });
     }
 
-    /**
-     * Display a dialog to enter a custom HS url.
-     */
-    private void displayCustomDirectoryDialog() {
-        AlertDialog.Builder alert = new AlertDialog.Builder(this);
-        View dialogView = LayoutInflater.from(this).inflate(R.layout.dialog_directory_picker, null);
-        alert.setView(dialogView);
-
-        final EditText editText = dialogView.findViewById(R.id.directory_picker_edit_text);
-
-        alert.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int whichButton) {
-                final String serverUrl = editText.getText().toString().trim();
-
-                if (!TextUtils.isEmpty(serverUrl)) {
-                    showWaitingView();
-                    mSession.getEventsApiClient().getPublicRoomsCount(serverUrl, new ApiCallback<Integer>() {
-                        @Override
-                        public void onSuccess(Integer count) {
-                            Intent intent = new Intent();
-                            intent.putExtra(EXTRA_OUT_ROOM_DIRECTORY_DATA, new RoomDirectoryData(serverUrl, serverUrl, null, null, false));
-                            setResult(RESULT_OK, intent);
-                            finish();
-                        }
-
-                        private void onError(String error) {
-                            Log.e(LOG_TAG, "## onSelectDirectoryServer() failed " + error);
-                            stopWaitingView();
-                            Toast.makeText(RoomDirectoryPickerActivity.this, R.string.directory_server_fail_to_retrieve_server, Toast.LENGTH_LONG).show();
-                        }
-
-                        @Override
-                        public void onNetworkError(Exception e) {
-                            onError(e.getMessage());
-                        }
-
-                        @Override
-                        public void onMatrixError(MatrixError e) {
-                            onError(e.getMessage());
-                        }
-
-                        @Override
-                        public void onUnexpectedError(Exception e) {
-                            onError(e.getMessage());
-                        }
-                    });
-                }
-            }
-        });
-
-        alert.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int whichButton) {
-            }
-        });
-
-        alert.show();
-    }
 
     /*
     * *********************************************************************************************
