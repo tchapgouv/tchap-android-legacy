78153dbf8322e9029337ef7a826956567ed6f6d2
audrey.olivier <audreyo@matrix.org>
2018-04-24 10:27:17 +0200

    Do not remove add homeserver button

--- vector/src/main/java/im/vector/activity/RoomDirectoryPickerActivity.java
+++ vector/src/main/java/im/vector/activity/RoomDirectoryPickerActivity.java
@@ -1,5 +1,6 @@
 /*
  * Copyright 2017 Vector Creations Ltd
+ * Copyright 2018 DINSIC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,14 +18,23 @@
 package im.vector.activity;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
+
 import org.matrix.androidsdk.util.Log;
+
+import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import org.matrix.androidsdk.MXSession;
 import org.matrix.androidsdk.rest.callback.ApiCallback;
@@ -110,11 +120,22 @@ public class RoomDirectoryPickerActivity extends RiotAppCompatActivity implement
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_directory_server_picker, menu);
+        CommonActivityUtils.tintMenuIcons(menu, ThemeUtils.getColor(this, R.attr.icon_tint_on_dark_action_bar_color));
+        return true;
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
             setResult(RESULT_CANCELED);
             finish();
             return true;
+        }
+
+        if (item.getItemId() == R.id.action_add_custom_hs) {
+            displayCustomDirectoryDialog();
         }
 
         return super.onOptionsItemSelected(item);
@@ -129,32 +150,34 @@ public class RoomDirectoryPickerActivity extends RiotAppCompatActivity implement
      * Refresh the directory servers list.
      */
     private void refreshDirectoryServersList() {
-       showWaitingView();
+        showWaitingView();
 
         mSession.getEventsApiClient().getThirdPartyServerProtocols(new ApiCallback<Map<String, ThirdPartyProtocol>>() {
             private void onDone(List<RoomDirectoryData> list) {
                 stopWaitingView();
+                String userHSName = mSession.getMyUserId().substring(mSession.getMyUserId().indexOf(":") + 1);
                 String userHSUrl = mSession.getHomeServerConfig().getHomeserverUri().getHost();
 
                 List<String> hsUrlsList = Arrays.asList(getResources().getStringArray(R.array.room_directory_servers));
 
                 int insertionIndex = 0;
 
+                // sanity check
+                // External users can not access to room directory
+                if (!LoginActivity.isUserExternal(mSession)) {
+                    // Add user's HS
+                    list.add(insertionIndex++, RoomDirectoryData.getIncludeAllServers(mSession, userHSUrl, userHSName));
 
-                // Add custom directory servers
-                for (String hsURL : hsUrlsList) {
-                    if (!TextUtils.equals(userHSUrl, hsURL)) {
-                        list.add(insertionIndex++, RoomDirectoryData.getIncludeAllServers(mSession, hsURL, hsURL));
+                    // Add custom directory servers
+                    for (String hsURL : hsUrlsList) {
+
+                        if (!TextUtils.equals(userHSUrl, "matrix." + hsURL)) {
+                            list.add(insertionIndex++, RoomDirectoryData.getIncludeAllServers(mSession, hsURL, hsURL));
+                        }
                     }
                 }
 
-                // sanity check
-                if (LoginActivity.isUserExternal(mSession)) {
-                    list.clear();
-                    mRoomDirectoryAdapter.updateDirectoryServersList(list);
-                } else {
-                    mRoomDirectoryAdapter.updateDirectoryServersList(list);
-                }
+                mRoomDirectoryAdapter.updateDirectoryServersList(list);
             }
 
             @Override
@@ -192,6 +215,63 @@ public class RoomDirectoryPickerActivity extends RiotAppCompatActivity implement
         });
     }
 
+    /**
+     * Display a dialog to enter a custom HS url.
+     */
+    private void displayCustomDirectoryDialog() {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        View dialogView = LayoutInflater.from(this).inflate(R.layout.dialog_directory_picker, null);
+        alert.setView(dialogView);
+
+        final EditText editText = dialogView.findViewById(R.id.directory_picker_edit_text);
+
+        alert.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                final String serverUrl = editText.getText().toString().trim();
+
+                if (!TextUtils.isEmpty(serverUrl)) {
+                    showWaitingView();
+                    mSession.getEventsApiClient().getPublicRoomsCount(serverUrl, new ApiCallback<Integer>() {
+                        @Override
+                        public void onSuccess(Integer count) {
+                            Intent intent = new Intent();
+                            intent.putExtra(EXTRA_OUT_ROOM_DIRECTORY_DATA, new RoomDirectoryData(serverUrl, serverUrl, null, null, false));
+                            setResult(RESULT_OK, intent);
+                            finish();
+                        }
+
+                        private void onError(String error) {
+                            Log.e(LOG_TAG, "## onSelectDirectoryServer() failed " + error);
+                            stopWaitingView();
+                            Toast.makeText(RoomDirectoryPickerActivity.this, R.string.directory_server_fail_to_retrieve_server, Toast.LENGTH_LONG).show();
+                        }
+
+                        @Override
+                        public void onNetworkError(Exception e) {
+                            onError(e.getMessage());
+                        }
+
+                        @Override
+                        public void onMatrixError(MatrixError e) {
+                            onError(e.getMessage());
+                        }
+
+                        @Override
+                        public void onUnexpectedError(Exception e) {
+                            onError(e.getMessage());
+                        }
+                    });
+                }
+            }
+        });
+
+        alert.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+            }
+        });
+
+        alert.show();
+    }
 
     /*
     * *********************************************************************************************
