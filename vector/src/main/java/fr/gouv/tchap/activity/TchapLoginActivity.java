e82b99a4303f4d7ea9ad04fd703bd086e0712bec
Arnaud Fontaine <arnaud.fontaine@ssi.gouv.fr>
2018-07-03 10:03:26 +0200

    Add some acceptable cipher suites + TLS v1.3

--- vector/src/main/java/fr/gouv/tchap/activity/TchapLoginActivity.java
+++ vector/src/main/java/fr/gouv/tchap/activity/TchapLoginActivity.java
@@ -2675,13 +2675,15 @@ public class TchapLoginActivity extends MXCActionBarActivity implements Registra
 
         res.setShouldAcceptTlsExtensions(false);
 
-        res.setAcceptedTlsVersions(Collections.singletonList(TlsVersion.TLS_1_2));
+        res.setAcceptedTlsVersions(Arrays.asList(TlsVersion.TLS_1_3, TlsVersion.TLS_1_2));
 
         res.setAcceptedTlsCipherSuites(Arrays.asList(
                 CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
                 CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256,
                 CipherSuite.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
                 CipherSuite.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256,
+                CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+                CipherSuite.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
                 CipherSuite.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
                 CipherSuite.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256));
 
