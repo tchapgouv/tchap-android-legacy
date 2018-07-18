d9835387fe050ff945185f2fd59054c81a4851de
Audrey OLIVIER <audreyo@matrix.org>
2018-07-18 15:32:11 +0200

    In the Conversation tab, do not show a room whose user has been excluded (kicked or banned). #349

--- vector/src/main/java/im/vector/activity/VectorRoomActivity.java
+++ vector/src/main/java/im/vector/activity/VectorRoomActivity.java
@@ -1651,14 +1651,6 @@ public class VectorRoomActivity extends MXCActionBarActivity implements MatrixMe
             mSearchInRoomMenuItem = menu.findItem(R.id.ic_action_search_in_room);
             mUseMatrixAppsMenuItem = null; //will be reconnected later menu.findItem(R.id.ic_action_matrix_apps);
 
-            if (null != mRoom) {
-                RoomMember member = mRoom.getMember(mSession.getMyUserId());
-                // kicked / banned room
-                if ((null != member) && member.kickedOrBanned()) {
-                    menu.findItem(R.id.ic_action_room_leave).setVisible(false);
-                }
-            }
-
             // hide / show the unsent / resend all entries.
             refreshNotificationsArea();
         }
