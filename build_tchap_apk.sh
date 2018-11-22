3322ea3cb87e4bd3929f9c2e1f1541169daa9b13
Giom Foret <giom@matrix.org>
2018-11-22 11:06:48 +0100

    Fix build_tchap_apk scripts

--- build_tchap_apk.sh
+++ build_tchap_apk.sh
@@ -6,5 +6,5 @@ rm *.apk
 ./gradlew lintAppfdroidWithVoipWithoutPinningRelease assembleAppfdroidWithVoipWithoutPinningMatrixorg
 
 #cp the resulting apk
-cp vector/build/outputs/apk/appWithVoip/matrixorg/vector-app-withVoip-withoutPinning-matrixorg.apk ./tchapGooglePlay.apk
-cp vector/build/outputs/apk/appfdroidWithVoip/matrixorg/vector-appfdroid-withVoip-withoutPinning-matrixorg.apk ./tchapFDroid.apk
+cp vector/build/outputs/apk/appWithVoipWithoutPinning/matrixorg/vector-app-withVoip-withoutPinning-matrixorg.apk ./tchapGooglePlay.apk
+cp vector/build/outputs/apk/appfdroidWithVoipWithoutPinning/matrixorg/vector-appfdroid-withVoip-withoutPinning-matrixorg.apk ./tchapFDroid.apk
--- build_tchap_without_voip_apk.sh
+++ build_tchap_without_voip_apk.sh
@@ -6,5 +6,5 @@ rm *.apk
 ./gradlew lintAppfdroidWithoutVoipWithoutPinningRelease assembleAppfdroidWithoutVoipWithoutPinningMatrixorg
 
 #cp resulting apk
-cp vector/build/outputs/apk/appWithoutVoip/matrixorg/vector-app-withoutVoip-withoutPinning-matrixorg.apk ./tchapWithoutVoipWithoutPinningGooglePlay.apk
-cp vector/build/outputs/apk/appfdroidWithoutVoip/matrixorg/vector-appfdroid-withoutVoip-withoutPinning-matrixorg.apk ./tchapWithoutVoipWithoutPinningFDroid.apk
+cp vector/build/outputs/apk/appWithoutVoipWithoutPinning/matrixorg/vector-app-withoutVoip-withoutPinning-matrixorg.apk ./tchapWithoutVoipWithoutPinningGooglePlay.apk
+cp vector/build/outputs/apk/appfdroidWithoutVoipWithoutPinning/matrixorg/vector-appfdroid-withoutVoip-withoutPinning-matrixorg.apk ./tchapWithoutVoipWithoutPinningFDroid.apk
