cd099fe7cf5532097dbe6878f37de2d41255f350
giomfo <giom@matrix.org>
2019-09-09 18:35:48 +0200

    Room Members: Allow to revoke 3pid invites (#530)
    * Room Members: Allow to revoke 3pid invites
    
    * Update matrix-sdk.aar libs - Revision: 268fe783bdcaa88920e9c696a995bed606de7ca3 [268fe783]

--- vector/src/main/java/im/vector/adapters/VectorRoomDetailsMembersAdapter.java
+++ vector/src/main/java/im/vector/adapters/VectorRoomDetailsMembersAdapter.java
@@ -395,9 +395,11 @@ public class VectorRoomDetailsMembersAdapter extends BaseExpandableListAdapter {
                     Collection<RoomThirdPartyInvite> thirdPartyInvites = mRoom.getState().thirdPartyInvites();
 
                     for (RoomThirdPartyInvite invite : thirdPartyInvites) {
-                        // If the home server has converted the 3pid invite into a room member, do not show it
-                        if (null == mRoom.getState().memberWithThirdPartyInviteToken(invite.token)) {
-                            ParticipantAdapterItem participant = new ParticipantAdapterItem(invite.display_name, "", null, true);
+                        // If the home server has converted the 3pid invite into a room member, do not show it.
+                        // If the invite has been revoked (null display name), ignore it too.
+                        if (null == mRoom.getState().memberWithThirdPartyInviteToken(invite.token)
+                                && invite.display_name != null) {
+                            ParticipantAdapterItem participant = new ParticipantAdapterItem(invite);
 
                             if ((!isSearchEnabled) || participant.contains(mSearchPattern)) {
                                 invitedMembers.add(participant);
@@ -909,7 +911,9 @@ public class VectorRoomDetailsMembersAdapter extends BaseExpandableListAdapter {
         });
 
         // SWIPE: the swipe should be enabled when there is no search and the user can kick other members
-        if (isSearchMode || isActionsMenuHidden || (null == participant.mRoomMember)) {
+        if (isSearchMode
+                || isActionsMenuHidden
+                || (null == participant.mRoomMember && null == participant.mRoomThirdPartyInvite)) {
             viewHolder.mSwipeCellLayout.setOnTouchListener(null);
         } else {
             viewHolder.mSwipeCellLayout.setOnTouchListener(new View.OnTouchListener() {
@@ -944,7 +948,7 @@ public class VectorRoomDetailsMembersAdapter extends BaseExpandableListAdapter {
                             if (Math.abs(x - mStartX) < 10) {
                                 // ignore the cancel event
                                 if (event.getAction() == MotionEvent.ACTION_UP) {
-                                    if (null != mOnParticipantsListener) {
+                                    if (null != mOnParticipantsListener && !TextUtils.isEmpty(participant.mUserId)) {
                                         mOnParticipantsListener.onClick(participant);
                                     }
                                 }
