d5ad46393bb2687cc5d90484caee4b4ba96fbca4
Giom Foret <giom@matrix.org>
2019-10-11 09:20:49 +0200

    Fix compilation errors

--- build_btchap_apk.sh
+++ build_btchap_apk.sh
@@ -7,14 +7,14 @@ rm *.apk
 output="vector/build/outputs/apk"
 
 # Google Play |  Preprod  | Without Voip | Without Pinning
-./gradlew assembleAppPreprodWithoutvoipWithoutpinningRelease;          cp ${output}/AppPreprodWithoutvoipWithoutpinning/matrixorg/vector-app-preprod-withoutvoip-withoutpinning-matrixorg.apk             ./tchapPreprodWithoutVoipGooglePlay.apk
+./gradlew assembleAppPreprodWithoutvoipWithoutpinningRelease;          cp ${output}/AppPreprodWithoutvoipWithoutpinning/release/vector-app-preprod-withoutvoip-withoutpinning-release.apk             ./tchapPreprodWithoutVoipGooglePlay.apk
 
 # Google Play |  Preprod  |   With Voip  | Without Pinning
-./gradlew assembleAppPreprodWithvoipWithoutpinningRelease;             cp ${output}/AppPreprodWithvoipWithoutpinning/matrixorg/vector-app-preprod-withvoip-withoutpinning-matrixorg.apk                   ./tchapPreprodWithVoipGooglePlay.apk
+./gradlew assembleAppPreprodWithvoipWithoutpinningRelease;             cp ${output}/AppPreprodWithvoipWithoutpinning/release/vector-app-preprod-withvoip-withoutpinning-release.apk                   ./tchapPreprodWithVoipGooglePlay.apk
 
 
 #    FDroid   |  Preprod  | Without Voip | Without Pinning
-./gradlew assembleAppfdroidPreprodWithoutvoipWithoutpinningRelease;    cp ${output}/AppfdroidPreprodWithoutvoipWithoutpinning/matrixorg/vector-appfdroid-preprod-withoutvoip-withoutpinning-matrixorg.apk       ./tchapPreprodWithoutVoipFDroid.apk
+./gradlew assembleAppfdroidPreprodWithoutvoipWithoutpinningRelease;    cp ${output}/AppfdroidPreprodWithoutvoipWithoutpinning/release/vector-appfdroid-preprod-withoutvoip-withoutpinning-release.apk       ./tchapPreprodWithoutVoipFDroid.apk
 
 #    FDroid   |  Preprod  |   With Voip  | Without Pinning
-./gradlew assembleAppfdroidPreprodWithvoipWithoutpinningRelease;       cp ${output}/AppfdroidPreprodWithvoipWithoutpinning/matrixorg/vector-appfdroid-preprod-withvoip-withoutpinning-matrixorg.apk             ./tchapPreprodWithVoipFDroid.apk
+./gradlew assembleAppfdroidPreprodWithvoipWithoutpinningRelease;       cp ${output}/AppfdroidPreprodWithvoipWithoutpinning/release/vector-appfdroid-preprod-withvoip-withoutpinning-release.apk             ./tchapPreprodWithVoipFDroid.apk
